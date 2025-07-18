55743f8824ba297f7b4fa5cbabf8a592068cabcf
radheesh1006 <159934100+radheesh1006@users.noreply.github.com>
2025-07-18 16:15:06 +0530

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   roots: ['<rootDir>/src'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)',  // <-- Add axios here to transform it
+  ],
   reporters: [
     'default',
     ['jest-junit', { outputDirectory: './frontend', outputName: 'frontend-test-results.xml' }]
