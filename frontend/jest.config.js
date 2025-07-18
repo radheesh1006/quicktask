627959c34e55ee66fd44d81fc2e193a07e4d76b0
radheesh1006 <159934100+radheesh1006@users.noreply.github.com>
2025-07-18 15:52:17 +0530

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  roots: ['<rootDir>/src'],  // Ensures tests in /src are discovered
+  roots: ['<rootDir>/src'],
   reporters: [
     'default',
     ['jest-junit', { outputDirectory: './frontend', outputName: 'frontend-test-results.xml' }]
